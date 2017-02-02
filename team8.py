51d921f435ba193ba2d3be4cb0039aca3aa4a6dc
JackJackerson <richhaga25382@ccps.org>
2017-02-02 13:24:00 -0500

    All of the code
    This is the finished copy of the Team 8 strategy

--- team8.py
+++ team8.py
@@ -10,6 +10,7 @@ team_name = 'Guambabwe' # Only 10 chars displayed.
 strategy_name = 'Blitzkrieg'
 strategy_description = 'Destroying the other teams with full force.'
 
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,15 +18,67 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    
-    
+    '''Count points here'''
     if my_history == 'c' and their_history == 'c':
         my_score += 650
+        
+    if their_history == 'b' and my_history == 'b':
+        my_score -= 250
+        
+    if my_history == 'b' and their_history == 'c':
+        my_score += 100
+        
+    if my_history == 'c' and their_history == 'b':
+        my_score -= 500
+        
+    '''Stop counting points here'''
     
+    '''first five rounds'''
+    if my_history == '': # This block will add variables and initialize their values for the first turn
+        number_of_turns = 0
+        my_score = 0
+        enemy_betray_streak = 0
+        enemy_collude_streak = 0
+        
+    if their_history == 'b': #Adds the streak function
+        enemy_betray_streak += 1
+        enemy_collude_streak = 0
+        
+    if their_history == 'c':
+        enemy_collude_streak += 1
+        enemy_betray_streak = 0
+        
+    if (number_of_turns % 5 == 0):
+        rando_choice = random.randint(1,2)
+        if rando_choice == 1:
+            return 'b'
+        elif rando_choice == 2:
+            return 'c'
+        else:
+            return 'b'
+    elif their_history == 'b' and enemy_betray_streak > 2:
+        number_of_turns += 1
+        return 'b'
+    elif their_history == 'c' and enemy_collude_streak > 3:
+        number_of_turns += 1
+        return 'b'
+    else:
+        number_of_turns += 1
+        return 'b' 
+        
+        
+        
+    if number_of_turns == 1 or number_of_turns == 2: #First five turns
+        number_of_turns +=1
+        return 'b'
+    elif number_of_turns == 3 or number_of_turns == 4:
+        number_of_turns += 1
+        return 'c'
     if their_history == '':
+        number_of_turns += 1
         return 'b'
         
-    if their_history == 'b':
+    
         
     
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
